ce6d99bfa2b59b0b7f393f9be7a62ae0ab0198ee
lucasnpc <lucasnpc2@outlook.com>
2024-01-21 15:12:02 -0300

    initial commit

---
+++ GifMaker_Swift_Template/SavedGifsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SavedGifsViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Lucas Lopes on 21/01/24.
+//  Copyright © 2024 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SavedGifsViewController: UIViewController{
+    
+}
